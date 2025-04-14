fb8055e85eb3747a3f8d36b6388a5b5a2659dc19
youngdecade26 <86234584+youngdecade26@users.noreply.github.com>
2025-04-14 18:43:31 +0530

    changes
    changes

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -50,7 +50,7 @@ const axios = require('axios');
 const sendOtp = async () => {
   try {
     const response = await axios.post(
-      `https://control.msg91.com/api/v5/otp?template_id=67e253a1d6fc050fad3baff4&mobile=7389534852&authkey=435272AT2B1NRQ67e38dbeP1&realTimeResponse=`,
+      `https://control.msg91.com/api/v5/otp?template_id=67e253a1d6fc050fad3baff4&mobile=${mobile}&authkey=435272AT2B1NRQ67e38dbeP1&realTimeResponse=`,
       {
         Param1: "value1",
         Param2: "value2",
